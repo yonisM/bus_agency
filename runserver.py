95eddd8a4d58a257d4281fa464e16e72f1ad44b6
yonis mohamoud <yonis838@googlemail.com>
2020-11-11 21:32:09 +0000

    first commit

---
+++ runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the recruitment_agency application using a development server.
+"""
+
+from os import environ
+from recruitment_agency import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
